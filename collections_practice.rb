ec933727f0a2c24f25353d793dc2d6096ba68f5c
Paris Dolfman <parisdolfman@gmail.com>
2018-06-18 18:55:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
 
+def sort_array_asc(array)
+ array.sort do |a, b|
+   a <=> b
+ end
+end
+
+def sort_array_desc(array)
+ array.sort do |a, b|
+   b <=> a
+ end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+ copy = array.reverse
+ return copy
+end
+
+def kesha_maker
+  array.each do |n|
+  n[2] = "$"  
+end
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def add_s
+
+end
